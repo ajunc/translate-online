33c9427a5febdda6460da5aca82bbcb9f759b08a
xubenjun <894012405@qq.com>
2018-11-10 21:37:49 +0800

    translate-online

---
+++ src/components/TranslateForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="row" id="translate">
+    <div class="col-md-6 col-md-offset-3">
+      <form id="transform" class="well form-inline" v-on:submit="formSubmit">
+        <input class="form-control" v-model='textToTranslate' type="text" name="" placeholder="输入要翻译的内容">
+        <select class="form-control" v-model="language">
+          <option value="en">English</option>
+          <option value="ru">Russian</option>
+          <option value="ko">Korean</option>
+          <option value="ja">Janpense</option>
+        </select>
+        <input class="btn" type="submit" name="翻译">
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TranslateForm',
+  data:function(){
+    return {
+      textToTranslate: '',
+      language: ''
+    }
+  },
+  methods: {
+    formSubmit: function(e){
+      //alert(this.textToTranslate);
+      this.$emit("formSubmit",this.textToTranslate,this.language)
+      e.preventDefault();
+    },
+    created:function(){
+      this.language = "en";
+    }
+  }
+}
+</script>
+
+<style>
+#transform{
+  border:1px solid #ccc;
+  border-radius: 10px;
+}
+</style>
---
+++ src/components/translateOutput.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="translateOutput">
+    <h2>translateOutput</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'translateOutput',
+  props:[
+    "translatedText"
+  ]
+  
+}
+</script>
+
+<style>
+
+</style>
